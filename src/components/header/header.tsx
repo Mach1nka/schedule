88af02b7a37cb38a91681496c9d26516217d4c7e
cahobad <36692205+cahobad@users.noreply.github.com>
2020-09-04 15:56:16 +0300

    init: task 5

---
+++ src/components/header/header.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { useSelector } from "react-redux";
+
+
+const Header: React.FC = () => {
+
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  );
+};
+
+export default Header;
--- src/components/pages/main-page.tsx
+++ src/components/pages/main-page.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
+import Header from "../header/header";
 
 const MainPage: React.FC = () => {
   return (
     <>
-      Hello!
+      <Header />
     </>
   );
 };
