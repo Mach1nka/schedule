318ba5e074c852524ab688f8c09b7fa0376848fa
cahobad <36692205+cahobad@users.noreply.github.com>
2020-09-04 16:43:30 +0300

    feat: add base structure for header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,34 @@
 import * as React from "react";
-import { useSelector } from "react-redux";
+import { Row, Col, Typography, Image, Radio } from "antd";
+
 
 
 const Header: React.FC = () => {
+  const { Title } = Typography;
+  const timezone = [];
+  for (let i = 0; i < 9; i++) {
+    timezone.push(<Option key={i}>{`GMT + ${i}`}</Option>);
+  }
+  console.log(timezone)
 
   return (
     <header>
-      <h1>header</h1>
+      <Row>
+        <Col span={8}>
+          <Image
+            width={83}
+            src="https://app.rs.school/static/images/logo-rsschool3.png"
+            alt="RSS logo"
+          />
+        </Col>
+        <Col span={8}><Title>Schedule</Title></Col>
+        <Col span={8}>
+          <Radio.Group defaultValue="student" size="large">
+            <Radio.Button value="student">Student</Radio.Button>
+            <Radio.Button value="mentor">Mentor</Radio.Button>
+          </Radio.Group>
+        </Col>
+      </Row>
     </header>
   );
 };
