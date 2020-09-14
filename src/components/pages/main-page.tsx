7a02eea00cb82e32a3501ab0ebe66e38c7a2a964
Dima <dima@onmaster.ru>
2020-09-14 21:18:20 +0300

    feat: Add user settings on start app

--- src/components/pages/main-page.tsx
+++ src/components/pages/main-page.tsx
@@ -4,13 +4,15 @@ import {MainDataContext} from "../../context/main-data-context";
 import useGettingEntity from "../../hooks/use-getting-entity/use-getting-entity";
 import {ReduxStateEntities} from "../../reducers/reducers-config";
 import CalendarView from "../calendar-view/calendar-view";
+import {userSettingsReducerData} from "../../reducers/root-reducer";
 
 const MainPage: React.FC = () => {
-
   const {
+    getUserSettings,
     getScheduleEvents,
   } = React.useContext(MainDataContext);
 
+  useGettingEntity({currentEntity: ReduxStateEntities.USER_SETTINGS, fetchFn: getUserSettings, data: userSettingsReducerData.initialState.data});
   useGettingEntity({currentEntity: ReduxStateEntities.SCHEDULE_EVENTS, fetchFn: getScheduleEvents});
 
   return (
