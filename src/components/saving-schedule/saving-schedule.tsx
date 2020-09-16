4c12b3f3bb8b513901cf812c2fbdd7df3e75843d
khvalkoilya <khvalko.ilya@gmail.com>
2020-09-16 21:36:16 +0300

    feat: add downloading schedule in pdf

--- src/components/saving-schedule/saving-schedule.tsx
+++ src/components/saving-schedule/saving-schedule.tsx
@@ -1,5 +1,24 @@
 import * as React from "react"
 import { Modal, Button } from 'antd';
+import { jsPDF } from "jspdf";
+import html2canvas from 'html2canvas';
+import CalendarView from '../calendar-view/calendar-view';
+
+
+
+function printDocument() {
+  const elem = document.querySelector('.ant-picker-calendar-full');
+  // elem.style.padding = '200px 100px 400px 100px';
+  html2canvas(elem)
+    .then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF();
+      pdf.addImage(imgData, 'JPEG', 0, 0, 210, 297);
+      pdf.output('dataurlnewwindow');
+      pdf.save("download.pdf");
+    })
+  ;
+}
 
 const savePDF = () => {
   alert('download pdf..')
@@ -21,7 +40,7 @@ const SavingSchedule: React.FC = () => {
           setVisible(false)
         }}
         footer={[
-          <Button key="back" onClick={()=>savePDF()}>
+          <Button key="back" onClick={()=>printDocument()}>
             To PDF
           </Button>,
           <Button key="submit" type="primary" onClick={(e)=>console.log(e.target)}>
