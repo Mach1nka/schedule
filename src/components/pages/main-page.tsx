ff9231db03fe6f3c662c65f98b2ae188efd27bed
Dmitri Siniakov <50990013+DzmitrySiniakou@users.noreply.github.com>
2020-09-18 13:01:28 +0300

    [feat]: added buttons for changing schedule view (#24)
    * init: add starting architecture of the project
    
    * feat: added buttons for switch shedule type
    
    * feat: added buttons for changing schedule view
    
    * feat: fixedsemantics issues
    
    Co-authored-by: Dima <dima@onmaster.ru>

--- src/components/pages/main-page.tsx
+++ src/components/pages/main-page.tsx
@@ -3,8 +3,10 @@ import Header from "../header/header";
 import {MainDataContext} from "../../context/main-data-context";
 import useGettingEntity from "../../hooks/use-getting-entity/use-getting-entity";
 import {ReduxStateEntities} from "../../reducers/reducers-config";
+import { SwitchScheduleView } from '../switch-schedule-view/switch-schedule-view';
 import CalendarView from "../calendar-view/calendar-view";
 import {userSettingsReducerData} from "../../reducers/root-reducer";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import SavingSchedule from '../saving-schedule/saving-schedule';
 
 const MainPage: React.FC = () => {
@@ -17,13 +19,15 @@ const MainPage: React.FC = () => {
   useGettingEntity({currentEntity: ReduxStateEntities.SCHEDULE_EVENTS, fetchFn: getScheduleEvents});
 
   return (
-    <>
-      <Header/>
-      <main>
-        <CalendarView/>
-        <SavingSchedule/>
-      </main>
-    </>
+    <Router>
+      <Route path='/'>
+        <Header/>
+        <main>
+          <SwitchScheduleView />
+          <CalendarView/>
+        </main>
+      </Route>
+    </Router>
   );
 };
 
---
+++ src/components/switch-schedule-view/sc.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const SwitchScheduleViewSC = {
+  UL: styled.ul`
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 20px;
+
+    @media only screen and (max-width: 480px) {
+      justify-content: center;
+      padding-inline-start: 0;
+    }
+  `,
+
+  LI: styled.li`
+    list-style-type: none;
+    margin-left: 7px;
+
+    @media only screen and (max-width: 480px) {
+      margin: 0 5px;
+    }
+  `,
+}
