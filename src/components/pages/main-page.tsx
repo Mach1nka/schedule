4d9bb59550cb15168ac54599abc0f556ba97529f
Mach1nka <50869190+Mach1nka@users.noreply.github.com>
2020-09-21 21:12:46 +0300

    [feat]: realise list and add routing (#27)
    * feat: install reqwest, add task-list
    
    * fix: change class to function
    
    * fix: fix path to task-list
    
    * feat: add collapse menu
    
    * fix: delete file sc.ts
    
    * fix: delete const
    
    * feat: pull from develop
    
    * feat: add date handler, output all data from backend in list
    
    * feat: add styles for shedule list
    
    * fix: fix border for list item
    
    * feat: add beginer  routing
    
    * fix: fix onClick function

--- src/components/pages/main-page.tsx
+++ src/components/pages/main-page.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
-import Header from "../header/header";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {userSettingsReducerData} from "../../reducers/root-reducer";
 import {MainDataContext} from "../../context/main-data-context";
 import useGettingEntity from "../../hooks/use-getting-entity/use-getting-entity";
 import {ReduxStateEntities} from "../../reducers/reducers-config";
 import { SwitchScheduleView } from '../switch-schedule-view/switch-schedule-view';
+import Header from "../header/header";
+import ScheduleList from "../schedule-list/schedule-list";
 import CalendarView from "../calendar-view/calendar-view";
-import {userSettingsReducerData} from "../../reducers/root-reducer";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import SavingSchedule from '../saving-schedule/saving-schedule';
 
 const MainPage: React.FC = () => {
@@ -23,10 +24,15 @@ const MainPage: React.FC = () => {
       <Route path='/'>
         <Header/>
         <main>
-          <SwitchScheduleView />
-          <CalendarView/>
+          <SwitchScheduleView/>
+          <Route path='/Calendar'>
+            <CalendarView/>
+          </Route>
+          <Route path='/List'>
+            <ScheduleList/>
+          </Route>
         </main>
-      </Route>
+      </Route> 
     </Router>
   );
 };
