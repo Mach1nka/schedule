78399f737fab33c8735aa52a04fbe9b7f3adb57c
Andey Amelchenia <pmad1986@gmail.com>
2020-09-22 13:46:07 +0300

    feat: add type events

---
+++ src/data/typeEvents.ts
@@ -0,0 +1,45 @@
+const typeEvents = [
+  {
+    name: 'Task',
+    type: 'task',
+    crossCheck: true,
+    descriptionUrl: true,
+    color: '',
+  },
+  {
+    name: 'Self Education',
+    type: 'selfEducation',
+    crossCheck: false,
+    descriptionUrl: false,
+    color: '',
+  },
+  {
+    name: 'YouTube Stream',
+    type: 'youTubeStream',
+    crossCheck: false,
+    descriptionUrl: false,
+    color: '',
+  },
+  {
+    name: 'Elective/YouTube Stream',
+    type: 'electiveYouTubeStream',
+    crossCheck: false,
+    descriptionUrl: false,
+    color: '',
+  },
+  {
+    name: 'Test',
+    type: 'test',
+    crossCheck: false,
+    descriptionUrl: false,
+    color: '',
+  },
+  {
+    name: 'New',
+    type: 'new',
+    crossCheck: true,
+    descriptionUrl: true,
+    color: '',
+  },
+];
+export default typeEvents;
