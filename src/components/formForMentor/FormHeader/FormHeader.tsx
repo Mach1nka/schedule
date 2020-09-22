3bc6de72cd21f7d7423c023f54913a6be61ae8b6
Andey Amelchenia <pmad1986@gmail.com>
2020-09-22 13:40:41 +0300

    fix: formheader

--- src/components/formForMentor/FormHeader/FormHeader.tsx
+++ src/components/formForMentor/FormHeader/FormHeader.tsx
@@ -1,33 +1,32 @@
 import React from 'react';
-import { Form, Input, Select } from 'antd';
+import { Form, Input } from 'antd';
 
 // import { UploadOutlined, InboxOutlined } from '@ant-design/icons';
 // import Input from './Input/Input';
 
-const FormHeader = (): React.ReactElement => {
-  const { Option } = Select;
+const FormHeader = ({ form }): React.ReactElement => {
   return (
     <>
       <Form.Item name="name" label="Name Task" rules={[{ required: true }]}>
         <Input allowClear />
       </Form.Item>
-      <Form.Item
-        name="type"
-        label="Type Task"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Select placeholder="Select a option and change input text above" allowClear>
-          <Option value="task">Task</Option>
-          <Option value="selfEducation">Self Education</Option>
-          <Option value="youTubeStream">YouTube Stream</Option>
-          <Option value="electiveYouTubeStream">Elective/YouTube Stream</Option>
-          <Option value="test">Test</Option>
-        </Select>
-      </Form.Item>
+      {form.getFieldValue('type') === 'New' ? (
+        <Form.Item name="type" label="Type Event" rules={[{ required: true }]}>
+          <Input allowClear />
+        </Form.Item>
+      ) : (
+        <Form.Item
+          name="type"
+          label="Type Event"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <span>{`${form.getFieldValue('type')}`}</span>
+        </Form.Item>
+      )}
     </>
   );
 };
