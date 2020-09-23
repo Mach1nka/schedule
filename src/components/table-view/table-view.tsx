9eb5ce148112fca13a16c7b128d697d0a4c30a09
Dima <dima@onmaster.ru>
2020-09-23 12:24:41 +0300

    refactor: hidden columns WIP

--- src/components/table-view/table-view.tsx
+++ src/components/table-view/table-view.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'antd/dist/antd.css';
-import { Table, Tag, Space } from 'antd';
-import { ColumnsType } from 'antd/es/table';
+import {Table, Tag} from 'antd';
+import {ColumnsType} from 'antd/es/table';
 
 import {useSelector} from "react-redux";
 import {selectScheduleEventsData} from "../../selectors/selectors";
-
 
 
 interface Columns {
@@ -22,6 +21,7 @@ const TableView: React.FC = () => {
   const scheduleEvents = useSelector(selectScheduleEventsData) || [];
   console.log(scheduleEvents);
 
+  const [hiddenColumns, setHiddenColumns] = useState<Set<string>>(new Set);
 
   const DateTimeFormat = {
     year: "numeric",
@@ -35,12 +35,43 @@ const TableView: React.FC = () => {
     return new Date(unixDate * 1000).toLocaleDateString('ru', settings);
   };
 
- 
-  
+  const handledFilter = (data: React.ChangeEvent<HTMLInputElement>): void => {
+    const value = data.currentTarget.value;
+    const checked = data.currentTarget.checked;
 
-  const columns: ColumnsType<Columns> = [
+    if (checked && hiddenColumns.has(value)) {
+      setHiddenColumns((prevState) => {
+        prevState.delete(value);
+        return new Set([...prevState]);
+      });
+    }
+
+    if (!checked && !hiddenColumns.has(value)) {
+      setHiddenColumns((prevState) => {
+        return new Set([...prevState, value]);
+      });
+    }
+  };
+
+  const filterComponent = () => {
+    return (
+      <div>
+        <button type="button">
+          F
+        </button>
+        <form>
+          <input type="checkbox" onChange={handledFilter} value="Start date" checked={!hiddenColumns.has("Start date")}/>
+          <label>Start date</label>
+          <input type="checkbox" onChange={handledFilter} value="Due date" checked={!hiddenColumns.has("Due date")}/>
+          <label>Due date</label>
+        </form>
+      </div>
+    );
+  };
+
+  const columnsSource: ColumnsType<Columns> = [
     {
-      title: 'set',
+      title: filterComponent,
       dataIndex: 'settings',
       key: 'settings',
     },
@@ -48,21 +79,20 @@ const TableView: React.FC = () => {
       title: 'Start date',
       dataIndex: 'startdate',
       key: 'startdate',
-      
     },
     {
       title: 'Due date',
       dataIndex: 'duedate',
       key: 'duedate',
     },
-    
+
     {
       title: 'Title',
       dataIndex: 'title',
       key: 'title',
       // вот здесь нужна помощь,
       // не могу айдишник передать в ссылку
-      render: (name, id)=> <a href={String(id)}>{name}</a>
+      render: (name, id) => <a href={String(id)}>{name}</a>
     },
     {
       title: 'Type',
@@ -106,10 +136,12 @@ const TableView: React.FC = () => {
     // },
   ];
 
+  const columns = columnsSource.filter((it) => it?.title && !hiddenColumns.has(it.title.toString()));
+
   if (scheduleEvents.length > 0) {
     const dataSource = [];
 
-    for (let i = 0; i < scheduleEvents.length; i+=1) {
+    for (let i = 0; i < scheduleEvents.length; i += 1) {
       const temp = {
         settings: String(i + 1),
         key: String(i),
@@ -117,13 +149,13 @@ const TableView: React.FC = () => {
         duedate: String(formatDateFromUnix(scheduleEvents[i].endDateTime, DateTimeFormat)),
         title: String(scheduleEvents[i].name),
         status: [String(scheduleEvents[i].type)],
-        
-        
+
+
       }
 
       dataSource.push(temp)
     }
-    
+
     console.log('дата')
     console.log(dataSource);
 
@@ -133,7 +165,7 @@ const TableView: React.FC = () => {
       </>
     );
 
-    
+
   }
 
   return (
