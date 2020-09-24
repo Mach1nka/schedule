e83a264b35fce8bb8ad68c32a775cbc4c5b132f2
Dzmitrysiniakou <jack.pott@mail.ru>
2020-09-24 15:09:58 +0300

    fix: deleted repeating module

--- src/components/pages/main-page.tsx
+++ src/components/pages/main-page.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import {userSettingsReducerData} from "../../reducers/root-reducer";
 import {MainDataContext} from "../../context/main-data-context";
 import useGettingEntity from "../../hooks/use-getting-entity/use-getting-entity";
 import {ReduxStateEntities} from "../../reducers/reducers-config";
@@ -9,7 +8,6 @@ import Header from "../header/header";
 import ScheduleList from "../schedule-list/schedule-list";
 import CalendarView from "../calendar-view/calendar-view";
 import {userSettingsReducerData} from "../../reducers/root-reducer";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import SavingSchedule from '../saving-schedule/saving-schedule';
 
 const MainPage: React.FC = () => {
