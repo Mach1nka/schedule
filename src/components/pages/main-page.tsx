3fa7c55a2a078606279a48bf2eaecb7e50fdb085
Dzmitrysiniakou <jack.pott@mail.ru>
2020-09-24 16:18:04 +0300

    fix: added path in mainpage

--- src/components/pages/main-page.tsx
+++ src/components/pages/main-page.tsx
@@ -1,15 +1,14 @@
 import * as React from "react";
-import Header from "../header/header";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import {MainDataContext} from "../../context/main-data-context";
 import useGettingEntity from "../../hooks/use-getting-entity/use-getting-entity";
 import {ReduxStateEntities} from "../../reducers/reducers-config";
 import { SwitchScheduleView } from '../switch-schedule-view/switch-schedule-view';
+import Header from "../header/header";
+import ScheduleList from "../schedule-list/schedule-list";
 import CalendarView from "../calendar-view/calendar-view";
 import {userSettingsReducerData} from "../../reducers/root-reducer";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import SavingSchedule from '../saving-schedule/saving-schedule';
-
-import TableView from '../table-view/table-view';
 
 const MainPage: React.FC = () => {
   const {
@@ -25,9 +24,16 @@ const MainPage: React.FC = () => {
       <Route path='/'>
         <Header/>
         <main>
-          <SwitchScheduleView />
-          <TableView />
-          <CalendarView/>
+          <SwitchScheduleView/>
+          <Route path='/List'>
+            <ScheduleList/>
+          </Route>
+          <Route path='/Table'>
+            <ScheduleList/>
+          </Route>
+          <Route path='/Calendar'>
+            <CalendarView/>
+          </Route>
         </main>
       </Route>
     </Router>
